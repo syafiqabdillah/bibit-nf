94318d0483125417fb3b2ed0f13e159546dd6683
syafiq <abdillah.syafiq@gmail.com>
2020-08-25 19:04:25 +0700

    init halaman home dan navbar

---
+++ src/fragments/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="nav">
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+      <b-navbar-brand>Bantu Bisnis Teman</b-navbar-brand>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>
+          <router-link to="/">Home</router-link>
+        </b-nav-item>
+        <b-nav-item>
+          <router-link to="/about">About</router-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+#nav a {
+  font-weight: bold;
+  color: #50A3B9;
+}
+
+#nav a.router-link-exact-active {
+  color: #ffffff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-jumbotron
+      header="Bantu Bisnis Teman"
+      bg-variant="info"
+      text-variant="white"
+      lead="Platform jualan antar teman"
+    >
+      <b-form>
+        <b-form-input
+          id="search-query"
+          v-model="searchQuery"
+          placeholder="Cari toko, produk, atau nama penjual"
+        ></b-form-input>
+      </b-form>
+    </b-jumbotron>
+
+    <div id="products">
+      <b-card-group columns>
+        <b-card v-for="product in computedProductList" :key="product">
+          <div align="left">
+            <h4>{{ product.namaProduk }}</h4>
+            <p>{{ product.namaToko }}</p>
+            <span>{{ product.harga }}</span>
+          </div>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data() {
+    return {
+      searchQuery: "",
+      productList: [
+        {
+          namaProduk: "Product 123",
+          namaToko: "Toko ABC",
+          harga: "Rp 30.000",
+        },
+        {
+          namaProduk: "Product 234",
+          namaToko: "Toko DEF",
+          harga: "Rp 40.000",
+        },
+        {
+          namaProduk: "Product 345",
+          namaToko: "Toko GHI",
+          harga: "Rp 50.000",
+        },
+        {
+          namaProduk: "Product 456",
+          namaToko: "Toko JKL",
+          harga: "Rp 60.000",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+        },
+      ],
+    };
+  },
+  computed: {
+    computedProductList() {
+      return this.productList.filter(product => {
+        return product.namaProduk.includes(this.searchQuery)
+      })
+    }
   }
-}
+};
 </script>
+
+<style>
+#products {
+  margin: 30px;
+}
+</style>
