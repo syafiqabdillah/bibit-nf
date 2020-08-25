b0a4aba0a27eed3cc252aa169f99f42c55139bb0
syafiq <abdillah.syafiq@gmail.com>
2020-08-25 20:16:11 +0700

    product list grid

--- src/fragments/Navbar.vue
+++ src/fragments/Navbar.vue
@@ -3,7 +3,7 @@
     <b-navbar toggleable="sm" type="dark" variant="dark">
       <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-      <b-navbar-brand>Bantu Bisnis Teman</b-navbar-brand>
+      <b-navbar-brand>BBT</b-navbar-brand>
 
       <b-navbar-nav class="ml-auto">
         <b-nav-item>
@@ -26,7 +26,7 @@ export default {
 <style scoped>
 #nav a {
   font-weight: bold;
-  color: #50A3B9;
+  color: #50a3b9;
 }
 
 #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,16 +15,22 @@
       </b-form>
     </b-jumbotron>
 
-    <div id="products">
-      <b-card-group columns>
-        <b-card v-for="product in computedProductList" :key="product">
-          <div align="left">
-            <h4>{{ product.namaProduk }}</h4>
-            <p>{{ product.namaToko }}</p>
-            <span>{{ product.harga }}</span>
-          </div>
-        </b-card>
-      </b-card-group>
+    <div class="product-container">
+      <b-card
+        class="product-item"
+        v-for="product in computedProductList"
+        :key="product"
+        img-src="https://picsum.photos/600/300/?image=25"
+      >
+        <div align="left">
+          <h4>{{ product.namaProduk }}</h4>
+          <span id="harga">{{ product.harga }}</span>
+          <h5>{{ product.namaToko }}</h5>
+          <b-card-text class="small text-muted"
+            >Last updated 3 mins ago</b-card-text
+          >
+        </div>
+      </b-card>
     </div>
   </div>
 </template>
@@ -46,42 +52,100 @@ export default {
           namaProduk: "Product 123",
           namaToko: "Toko ABC",
           harga: "Rp 30.000",
+          lokasi: "Bontang",
         },
         {
           namaProduk: "Product 234",
           namaToko: "Toko DEF",
           harga: "Rp 40.000",
+          lokasi: "Bontang",
         },
         {
           namaProduk: "Product 345",
           namaToko: "Toko GHI",
           harga: "Rp 50.000",
+          lokasi: "Bontang",
         },
         {
           namaProduk: "Product 456",
           namaToko: "Toko JKL",
           harga: "Rp 60.000",
+          lokasi: "Bontang",
         },
         {
           namaProduk: "Product 567",
           namaToko: "Toko MNO",
           harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
+        },
+        {
+          namaProduk: "Product 567",
+          namaToko: "Toko MNO",
+          harga: "Rp 70.000",
+          lokasi: "Bontang",
         },
       ],
     };
   },
   computed: {
     computedProductList() {
-      return this.productList.filter(product => {
-        return product.namaProduk.includes(this.searchQuery)
-      })
-    }
-  }
+      return this.productList.filter((product) => {
+        return product.namaProduk.includes(this.searchQuery);
+      });
+    },
+  },
 };
 </script>
 
 <style>
 #products {
   margin: 30px;
+}
+#harga {
+  font-size: 14px;
+  font-weight: bold;
+  color: rgb(250, 89, 29);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  line-height: 22px;
+}
+.product-container {
+  display: grid;
+  grid-template-columns: 20% 20% 20% 20% 20%;
+  grid-auto-rows: 300px;
+  margin: 30px;
+}
+.product-item {
+  margin: 10px;
 }
 </style>
